7859ef3157a54a22be5586115d99589d0875e619
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 19:51:47 -0300

    Incluindo o novo servico de listar todos

--- src/main/java/com/mmenezes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/mmenezes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,7 +1,10 @@
-package com.mmenezes.rest.webservices.restfulwebservices;
+package com.mmenezes.rest.webservices.restfulwebservices.helloworld;
+
+import javax.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,12 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world-bean")
 	public MessageBean helloWorldBean() {
-		return new MessageBean("Hello world");
+		return new MessageBean("Hello world Changed s");
+		//throw new RuntimeException("Algum erro ocorreu");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public MessageBean helloWorldBean(@PathVariable String name) {
+		return new MessageBean(String.format("Hello world Changed %s", name));
 	}
 }
