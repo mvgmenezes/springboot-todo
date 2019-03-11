44cfb864a6334b445e30926f2eb09659b774a7d3
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-11 05:52:07 -0300

    Incluindo JPA no projeto

--- src/main/java/com/mmenezes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
+++ src/main/java/com/mmenezes/rest/webservices/restfulwebservices/todo/TodoHardcodedService.java
@@ -11,7 +11,7 @@ public class TodoHardcodedService {
 	
 	private static List<Todo> todos = new ArrayList<>();
 	
-	private static int idCounter = 0;
+	private static long idCounter = 0;
 	
 	
 	static {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values (10001, 'mmenezes', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10002, 'mmenezes', 'Learn Dance', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10003, 'mmenezes', 'Learn Microservices', sysdate(), false);
